641fef326d2064c6e3ac4a7aa46a15bf7d83378f
zhoulk <849693228@qq.com>
2020-10-20 11:47:45 +0800

    20201020

---
+++ UnityAndroidBridge/UnityBridge/Assets/Scripts/AndroidBridge.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidBridge : MonoBehaviour
+{
+    void HelloWorld(string name)
+    {
+        Debug.Log("hello " + name);
+    }
+}
---
+++ UnityAndroidBridge/UnityBridge/Assets/Scripts/UnityBridge.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityBridge : MonoBehaviour
+{
+    private AndroidJavaObject jo;
+
+    private void Awake()
+    {
+        AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+    }
+
+    void Start()
+    {
+        jo.Call("HelloWorld", "lufy");
+    }
+}
